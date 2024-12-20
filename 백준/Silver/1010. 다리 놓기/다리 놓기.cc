0e157456d3de7c2dab62c794ff0af8d62e346789
김수빈 <85053365+u1qns@users.noreply.github.com>
2024-12-21 00:09:14 +0900

    [Silver V] Title: 다리 놓기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.cc
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.cc
@@ -1,22 +1,32 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
+int dp[31][31] = {0, };
+
+int comb(const int n, const int r)
+{     
+        if (r < 0 || r > n) // r이 n보다 크거나 음수일 경우
+        return 0;
+    if (r == 0 || r == n) // r이 0이거나 n과 같을 경우
+        return 1;
+          
+    if(dp[n][r] != 0)
+        return dp[n][r];
+        
+    return dp[n][r] = comb(n-1, r-1) + comb(n-1, r);
+}
 
 int main () {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     
-    int tcase, n, r, temp;
-    int answer;
-
-    cin >> tcase;
-    while(tcase--) {
-        answer = 1;
-        temp = 1;
-        cin >> r >> n;
-        for(int i = n; i > n - r; i--) {
-            answer *= i;
-            answer /= temp++;
-        }
-        cout << answer << endl;
+    int T, N, M;
+    for(int i=0; i<31; ++i)
+        dp[i][0] = dp[i][i] = 1; // 초기값 세팅
+    
+    cin >> T;
+    while(T--)
+    {
+        cin >> N >> M;
+        cout << comb(M, N) << "\n"; // 여길 주의!
     }
 }
