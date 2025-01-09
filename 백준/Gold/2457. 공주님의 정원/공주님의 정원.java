8876a5e4333060c12700f0804d4c1b98a5288fcc
김수빈 <85053365+u1qns@users.noreply.github.com>
2025-01-09 17:43:39 +0900

    [Gold III] Title: 공주님의 정원, Time: 508 ms, Memory: 53756 KB -BaekjoonHub

--- 백준/Gold/2457. 공주님의 정원/공주님의 정원.java
+++ 백준/Gold/2457. 공주님의 정원/공주님의 정원.java
@@ -9,62 +9,52 @@ public class Main {
         return DAYS[month - 1] + day;
     }
 
-    public static void main(String[] args) throws NumberFormatException, IOException {
+    static PriorityQueue<int[]> flowers = new PriorityQueue<>((a, b) -> 
+        a[0] == b[0] ? Integer.compare(a[1], b[1]) : Integer.compare(a[0], b[0])
+    );
+
+    static int solve(final int start, final int end) {
+        int answer = 0;
+        int now = start;
+
+        while(!flowers.isEmpty() && now <= end) {
+            int max_end = -1;
+
+            while(!flowers.isEmpty() && now >= flowers.peek()[0]) {
+                max_end = Math.max(max_end, flowers.poll()[1]);
+            }
+
+            if(max_end == -1) {
+                return 0;
+            }
+
+            ++answer;
+            now = max_end;
+        }
+
+        return now < 335 ? 0 : answer;
+    }
+
+    public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int answer = 0;
-        int N = Integer.parseInt(st.nextToken());
-        List<int[]> range = new ArrayList<int[]>();
 
         for (int i = 1; i < 13; ++i) {
             DAYS[i] += DAYS[i - 1];
         }
+
         int ts = trans(3, 1);
         int te = trans(11, 30);
+        int N = Integer.parseInt(st.nextToken());  
+
         for (int i = 0; i < N; ++i) {
             st = new StringTokenizer(br.readLine());
-            int a = trans(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
-            int b = trans(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
-            range.add(new int[]{a, b});
+            int start = trans(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+            int end = trans(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+            flowers.offer(new int[]{start, end});
         }
-
-        Collections.sort(range, (a, b) -> {
-            if (a[0] == b[0]) return Integer.compare(a[1], b[1]);
-            return Integer.compare(a[0], b[0]);
-        });
-
-        int start, end, max_end = -1;
-
-        // ts 보다 작은 start를 가지는 것들 중에서 가장 큰 end 값 가져오기
-        for (int i = 0; i < N; ++i) {
-            
-            if (te < ts) break;
-
-            max_end = -1;
-            for (int j = i; j < N; ++j) {
-                start = range.get(j)[0];
-                end = range.get(j)[1];
-
-                if (start > ts) break;
-
-                if (max_end < end) max_end = end;
-                i = j;
-
-            }
-
-            if (max_end != -1) {
-                ++answer;
-                ts = max_end;
-            } else {
-                answer = 0;
-                break;
-            }
-        }
-
-
-        System.out.print(ts < 335 ? 0 : answer);
+    
+        System.out.print(solve(ts, te));
     }
 }
