8a52d02fd1a12caced119c059dc74ec2b3e4c6ef
김수빈 <85053365+u1qns@users.noreply.github.com>
2025-02-19 16:46:41 +0900

    [Gold III] Title: 스티커 붙이기, Time: 4 ms, Memory: 2076 KB -BaekjoonHub

--- 백준/Gold/18808. 스티커 붙이기/스티커 붙이기.cc
+++ 백준/Gold/18808. 스티커 붙이기/스티커 붙이기.cc
@@ -9,21 +9,17 @@ typedef pair<int, int> pii;
 
 constexpr int MAX_N = 40 + 1;
 constexpr int MAX_M = 10 + 1;
-constexpr int dx[] = {};
-constexpr int dy[] = {};
 
 int N, M, K, answer = 0;
 int grid[MAX_N][MAX_N] = {false, };
+int tmp_sticker[MAX_M][MAX_M] = {false, };
+
+
 struct Sticker
 {
     int r, c;
     int size = 0;
-    vector<vector<int>> grid;
-    Sticker()
-    {
-        grid.resize(MAX_M, vector<int>(MAX_N, 0));
-    }
-
+    int grid[MAX_M][MAX_M] = {0, };
 } stickers[100];
 
 bool isValid(const int x, const int y)
@@ -51,7 +47,6 @@ bool try_attach(const pii& start, const Sticker& target)
     {
         for(int j=0; j<target.c; ++j)
         {
-            // 노트북에 스티커가 붙여져 있는데 나도 붙여야할때..
             if(grid[i + start.first][j + start.second] && target.grid[i][j])
                 return false;
         }
@@ -73,14 +68,14 @@ void painting(const pii& start, const Sticker& target)
 
 }
 
-bool attach(const int idx)
+bool attach(Sticker& target)
 {
-    const Sticker& target = stickers[idx];
     for(int i=0; i<N; ++i)
     {
         for(int j=0; j<M; ++j)
         {
-            if(!isValid(i + target.r - 1, j + target.c -1)) continue;
+            if(!isValid(i + target.r - 1, j + target.c -1))
+                break;
 
             if(try_attach(make_pair(i, j), target))
             {
@@ -90,64 +85,43 @@ bool attach(const int idx)
             }
         }
     }
+
     return false;
 }
 
-void rotation90(const int sticker_idx)
+void rotation90(Sticker& target)
 {
-    auto& org = stickers[sticker_idx];
-    vector<vector<int>> result(MAX_M, vector<int>(MAX_M, 0));
-
-    for(int i=0; i<org.r; ++i)
+    for(int i=0; i<target.r; ++i)
     {
-        for(int j=0; j<org.c; ++j)
+        for(int j=0; j<target.c; ++j)
         {
-            result[j][org.r - i - 1] = org.grid[i][j];
+            tmp_sticker[j][target.r - i - 1] = target.grid[i][j];
         }
     }
 
-    std::swap(org.r, org.c);
-    for(int i=0; i<org.r; ++i)
+    std::swap(target.r, target.c);
+    for(int i=0; i<target.r; ++i)
     {
-        for(int j=0; j<org.c; ++j)
+        for(int j=0; j<target.c; ++j)
         {
-            stickers[sticker_idx].grid[i][j] = result[i][j];
+            target.grid[i][j] = tmp_sticker[i][j];
         }
     }
 }
 
 void solve()
 {
-    for(int t=0; t<K; ++t)
+    for(auto& target : stickers)
     {
         for(int d=0; d<4; ++d)
         {
-            if(attach(t))// t번째 스티커를 d방향으로 붙이려고 합니다
+            if(attach(target))// t번째 스티커를 d방향으로 붙이려고 합니다
                 break;
-            rotation90(t); // 안되면 돌려 - 한번 붙이고 말거라 그냥 덮어씀
-        }
-//        cout << "@grid status\n";
-//        show();
 
-    }
-}
-
-void test()
-{
-    cout << "rotation 90 test\n";
-    for(int i=0; i<K; ++i)
-    {
-        auto& target = stickers[i];
-        rotation90(i);
-        for(int i=0; i<target.r; ++i)
-        {
-            for(int j=0; j<target.c; ++j)
-                cout << target.grid[i][j] << " ";
-            cout << "\n";
+            if(d == 3) break;
+            rotation90(target); // 안되면 돌려 - 한번 붙이고 말거라 그냥 덮어씀
         }
-        cout << "\n=========================\n";
     }
-    cout << "\n";
 }
 
 int main()
