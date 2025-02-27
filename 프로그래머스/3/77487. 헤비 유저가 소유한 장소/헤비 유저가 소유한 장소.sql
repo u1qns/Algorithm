41abef8da2643972fdd6b9752160538e34ef62d5
김수빈 <85053365+u1qns@users.noreply.github.com>
2025-02-27 18:12:17 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
+++ 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -1,9 +1,9 @@
--- 코드를 입력하세요
-SELECT * 
-from places 
-where host_id IN (
+select ID, NAME, HOST_ID
+FROM places
+WHERE host_id IN (
     select host_id
     from places
     group by host_id
-    having count(*) > 1)
+    having count(*) > 1
+)
 order by id;
