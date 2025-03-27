083fee8ec327efb0f6db156559ef16e04d252592
김수빈 <85053365+u1qns@users.noreply.github.com>
2025-03-27 23:27:49 +0900

    [Bronze IV] Title: $A + B - C$, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+string A, B, C;
+
+int main()
+{
+    cin >> A >> B >> C;
+    cout << stoi(A) + stoi(B) - stoi(C) << "\n";
+    cout << stoi(A + B) - stoi(C);
+    return 0;
+}
