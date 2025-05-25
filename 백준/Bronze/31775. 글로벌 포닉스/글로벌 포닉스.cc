dd74b2a5148f193b9c71295413de489e2a14a4bd
김수빈 <85053365+u1qns@users.noreply.github.com>
2025-05-25 23:36:38 +0900

    [Bronze IV] Title: 글로벌 포닉스, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/31775. 글로벌 포닉스/글로벌 포닉스.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool solve()
+{
+    string str;
+    bool check[3] = {false, };
+    for(int i=0; i<3; ++i)
+    {
+        cin >> str;
+        if(str[0] == 'l')
+            check[0] = true;
+        else if(str[0] == 'k')
+            check[1] = true;
+        else if(str[0] == 'p')
+            check[2] = true;
+        else
+            return false;
+    }
+    
+    return check[0] && check[1] && check[2];
+}
+int main() 
+{
+    cout << (solve() ? "GLOBAL" : "PONIX");
+    return 0;
+}
