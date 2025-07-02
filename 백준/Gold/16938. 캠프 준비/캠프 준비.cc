23d6547629969026c9ba6606075d3ba971212d10
김수빈 <85053365+u1qns@users.noreply.github.com>
2025-07-02 23:58:13 +0900

    [Gold V] Title: 캠프 준비, Time: 0 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Gold/16938. 캠프 준비/캠프 준비.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int N, L, R, X;
+int answer = 0;
+int A[1000001];
+
+void solve(int cnt, int idx, int sum, int easy, int hard)
+{
+    if(cnt >= 2 && sum >= L && sum <= R && (hard - easy) >= X)
+    {
+        ++answer;
+    }
+
+    for(int i=idx; i<N; ++i)
+    {
+        int e = (A[i] < easy ? A[i] : easy);
+        int h = (A[i] > hard ? A[i] : hard);
+        solve(cnt+1, i+1, sum + A[i], e, h);
+    }
+}
+
+
+int main()
+{
+    cin >> N >> L >> R >> X;
+
+    for(int i=0; i<N; ++i)
+    {
+        cin >> A[i];
+    }
+
+    solve(0, 0, 0, 1e9, 0);
+
+    cout << answer;
+
+    return 0;
+}
