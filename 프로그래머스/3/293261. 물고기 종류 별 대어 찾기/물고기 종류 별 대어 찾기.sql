6bed0b49ebe0822c210c6b1eca4ee217cca7c37e
김수빈 <85053365+u1qns@users.noreply.github.com>
2025-10-26 19:34:12 +0900

    [level 3] Title: 물고기 종류 별 대어 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
+++ 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
@@ -1,13 +1,14 @@
 -- 물고기 종류 별로 가장 큰 물고기의 ID, 물고기 이름, 길이를 출력
--- 물고기의 ID에 대해 오름차순 정렬
+-- 물고기의 ID에 대해 오름차순 정렬해주세요.
 
-SELECT ID, FISH_NAME, LENGTH
-FROM FISH_INFO i JOIN FISH_NAME_INFO ni 
-    ON i.FISH_TYPE = ni.FISH_TYPE
-WHERE (i.FISH_TYPE, i.LENGTH) IN 
-    (
-        SELECT FISH_TYPE, MAX(LENGTH)
-        FROM FISH_INFO
-        GROUP BY FISH_TYPE
-    )
-ORDER BY i.ID;
+    
+SELECT ID, FISH_NAME, i.LENGTH
+FROM FISH_INFO i JOIN  (
+    SELECT FISH_TYPE, MAX(LENGTH) AS LENGTH
+    FROM FISH_INFO i
+    GROUP BY FISH_TYPE
+) tmp 
+ ON (i.FISH_TYPE, i.LENGTH) = (tmp.FISH_TYPE, tmp.LENGTH)
+
+JOIN FISH_NAME_INFO n ON i.FISH_TYPE = n.FISH_TYPE
+ORDER BY ID
